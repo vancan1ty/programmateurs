9bc2687a14293825c6289dd43eb0307e4801434a
Sara-Cagle <cagle.sara@gmail.com>
2014-01-22 14:42:48 -0500

    Edited Person4.java calc method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -1,4 +1,4 @@
-package edu.gatech.oad.antlab.person;
+pckage edu.gatech.oad.antlab.person;
 
 /**
  *  A simple class for person 1
@@ -30,8 +30,12 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 1 put your implementation here
-      return null;
+    	char[] charArray = input.toCharArray();
+    	String ans ="";
+    	for(char character: charArray){
+    		ans=ans+(char)(character+1);
+    	}
+      return ans;
     }
     
     /**
@@ -45,6 +49,56 @@ public class Person4 {
     public String toString(String input) {
       return name + calc(input);
     }
+
+}
+
+ackage edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 1
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person4 {
+	/** Holds the persons real name */
+	private String name;
+	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	public Person4(String pname) {
+		name = pname;
+	}
+	/**
+	 * This method should return a string
+	 * where each character is 1 greater 
+	 * than its previous value.  So
+	 * given "abc123" it should return
+	 * "bcd234".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+		//Person 1 put your implementation here
+		return null;
+	}
+
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+		return name + calc(input);
+	}
 
 }
 
