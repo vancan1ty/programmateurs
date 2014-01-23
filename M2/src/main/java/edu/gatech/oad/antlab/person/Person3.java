6f6b4b127f39fe9cdfad5cb323ec25a775df4d55
Justin Nieto <Nieto84@gmail.com>
2014-01-22 20:53:47 -0500

    Completed calc() method in Person3.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+        String reverse = "";
+        for(int i=input.length() -1 ;i>=0;i--){
+            reverse += input.charAt(i);
+        }
+        return reverse;
 	}
 }
