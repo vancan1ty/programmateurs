6fdbc140adaef0e74b040d76f11615cea7afcb02
pavelkomarov <pkomarov@gatech.edu>
2014-03-02 13:37:43 -0500

    updated some of the strings

--- cashmoneypro/src/programmateurs/views/HomeActivity.java
+++ cashmoneypro/src/programmateurs/views/HomeActivity.java
@@ -129,11 +129,11 @@ public class HomeActivity extends FragmentActivity {
 			Locale l = Locale.getDefault();
 			switch (position) {
 			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
+				return getString(R.string.title_accounts).toUpperCase(l);
 			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
+				return getString(R.string.title_history).toUpperCase(l);
 			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
+				return getString(R.string.title_reports).toUpperCase(l);
 			}
 			return null;
 		}
