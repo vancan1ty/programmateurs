13058e6e45846ce78630d7710cae212f36fe2ad3
pavelkomarov <pkomarov@gatech.edu>
2014-04-02 14:48:37 -0400

    checkstyle things

--- cashmoneypro/src/programmateurs/views/CategoryActivity.java
+++ cashmoneypro/src/programmateurs/views/CategoryActivity.java
@@ -6,8 +6,6 @@ import programmateurs.interfaces.DataSourceInterface;
 import programmateurs.models.Anchor;
 import programmateurs.models.RealDataSource;
 import net.programmateurs.R;
-import net.programmateurs.R.layout;
-import net.programmateurs.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -23,12 +21,13 @@ import android.widget.Spinner;
 /**
  * CategoryActivity is the screen in which users can add or delete categories.
  * it is accessed through the Options Menu in HomeActivity.
- * 
+ *
  * @author Justin
  * @version 0.1
  */
-public class CategoryActivity extends Activity implements OnItemSelectedListener {
-	private EditText categoryNameField;
+public class CategoryActivity extends Activity implements
+    OnItemSelectedListener {
+    private EditText categoryNameField;
 	private Button addButton;
 	private Button deleteButton;
 	private Spinner categorySpinner;
@@ -38,12 +37,12 @@ public class CategoryActivity extends Activity implements OnItemSelectedListener
 	private User user;
 	private Category[] categories;
 	private Category toDelete;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_category);
-		
+
 		dbHandler = new RealDataSource(this);
 		anchor = Anchor.getInstance();
 		categoryNameField = (EditText) findViewById(R.id.name_field);
@@ -107,20 +106,17 @@ public class CategoryActivity extends Activity implements OnItemSelectedListener
 		populateSpinner();
 	}
 	
-	@Override
-	public void onDestroy(){
+    @Override
+    public void onDestroy(){
 		dbHandler.close();
 		super.onDestroy();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.category, menu);
 		return true;
-	}
-	
-	
-	
+    }
 
 }
