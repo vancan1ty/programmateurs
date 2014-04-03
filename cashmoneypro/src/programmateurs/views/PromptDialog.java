8970c8701d8ca385e48fd27cdb71b99dd3df3da2
Sara-Cagle <cagle.sara@gmail.com>
2014-04-02 21:58:39 -0400

    PromptDialog checkstyle save for one

--- cashmoneypro/src/programmateurs/views/PromptDialog.java
+++ cashmoneypro/src/programmateurs/views/PromptDialog.java
@@ -7,57 +7,71 @@ import android.content.DialogInterface.OnClickListener;
 import android.widget.EditText;
 
 /**
- * helper for Prompt-Dialog creation
+ * helper for Prompt-Dialog creation.
  */
 public abstract class PromptDialog extends AlertDialog.Builder implements
 		OnClickListener {
-	public final EditText input;
+	//CHECKSTYLE:OFF
+    public final EditText input;
+    //CHECKSTYLE:ON
 
 	/**
-	 * @param context
+	 * Prompt Dialog.
+	 * 
+	 * @param context context
+	 * @param title title
+	 * @param message message
 	 */
-	public PromptDialog(Context context, String title, String message) {
+    public PromptDialog(Context context, String title, String message) {
 		super(context);
-		setTitle(title);
-		setMessage(message);
+        setTitle(title);
+        setMessage(message);
 
-		input = new EditText(context);
-		setView(input);
+        input = new EditText(context);
+        setView(input);
 
-		setPositiveButton("ok", this);
-		setNegativeButton("cancel", this);
-	}
+        setPositiveButton("ok", this);
+        setNegativeButton("cancel", this);
+    }
 
 	/**
 	 * will be called when "cancel" pressed. closes the dialog. can be
 	 * overridden.
 	 * 
-	 * @param dialog
+	 * @param dialog dialog
 	 */
-	public void onCancelClicked(DialogInterface dialog) {
-		dialog.dismiss();
-	}
+    public void onCancelClicked(DialogInterface dialog) {
+        dialog.dismiss();
+    }
 
-	@Override
-	public void onClick(DialogInterface dialog, int which) {
-		if (which == DialogInterface.BUTTON_POSITIVE) {
-			if (onOkClicked(input.getText().toString())) {
-				dialog.dismiss();
-			}
-		} else {
-			onCancelClicked(dialog);
-		}
-	}
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            if (onOkClicked(input.getText().toString())) {
+                dialog.dismiss();
+            }
+        }
+        else {
+            onCancelClicked(dialog);
+        }
+    }
 
-	public String getText() {
-		return input.getText().toString();
-	}
+    /**
+     * Get text.
+     * 
+     * @return text text
+     */
+    public String getText() {
+        return input.getText().toString();
+    }
 
 	/**
 	 * called when "ok" pressed.
 	 * 
-	 * @param input
+	 * @param input input
 	 * @return true, if the dialog should be closed. false, if not.
 	 */
-	abstract public boolean onOkClicked(String input);
+    //CHECKSTYLE:OFF
+    public abstract boolean onOkClicked(String input);
+    //CHECKSTYLE:ON
 }
