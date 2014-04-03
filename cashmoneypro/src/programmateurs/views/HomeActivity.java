c8463c5dd1588ba8e96eb0dee1252f20f764b625
Sara-Cagle <cagle.sara@gmail.com>
2014-04-02 21:33:03 -0400

    HomeActivity checkstyle done

--- cashmoneypro/src/programmateurs/views/HomeActivity.java
+++ cashmoneypro/src/programmateurs/views/HomeActivity.java
@@ -3,13 +3,6 @@ package programmateurs.views;
 import java.util.Locale;
 
 import net.programmateurs.R;
-import net.programmateurs.R.id;
-import net.programmateurs.R.layout;
-import net.programmateurs.R.menu;
-import net.programmateurs.R.string;
-
-import programmateurs.beans.Account;
-import programmateurs.beans.User;
 import programmateurs.models.Anchor;
 
 import android.content.Intent;
@@ -18,24 +11,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * The class sets up the main screen when the user logs in It has the
- * SectionsPagerAdapter, which allows the user to scroll to new tabs
+ * SectionsPagerAdapter, which allows the user to scroll to new tabs.
  * 
- * @author currell
+ * @author Currell
  * @version 0.0
  * 
  */
@@ -49,131 +37,143 @@ public class HomeActivity extends FragmentActivity {
 	 * intensive, it may be best to switch to a
 	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
 	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+	
+	//CHECKSTYLE:OFF
+    SectionsPagerAdapter mSectionsPagerAdapter;
 
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
-	ViewPager mViewPager;
-	Anchor anchor;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
-		anchor = Anchor.getInstance();
-		this.setTitle(anchor.getCurrentUser().getFirst());
+    ViewPager mViewPager;
+    Anchor anchor;
+    String logout0 = "Log Out";
+    //CHECKSTYLE:ON
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        anchor = Anchor.getInstance();
+        this.setTitle(anchor.getCurrentUser().getFirst());
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
+        mSectionsPagerAdapter = new SectionsPagerAdapter(
 				getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-	}
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuItem settings = menu.add("Settings");
-		settings.setIntent(new Intent(this, SettingsActivity2.class));
-		MenuItem logout = menu.add("Log Out");
-		logout.setIntent(new Intent(this, WelcomeActivity.class));
-		MenuItem categories = menu.add("Edit Categories");
-		categories.setIntent(new Intent(this, CategoryActivity.class));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem settings = menu.add("Settings");
+        settings.setIntent(new Intent(this, SettingsActivity2.class));
+        
+        MenuItem logout = menu.add(logout0);
+        logout.setIntent(new Intent(this, WelcomeActivity.class));
+        MenuItem categories = menu.add("Edit Categories");
+        categories.setIntent(new Intent(this, CategoryActivity.class));
 		// getMenuInflater().inflate(R.menu.home, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 		// Log.d("HomeActivity","Title: " + item.getTitle());
-		if (item.getTitle().equals("Log Out")) {
+        if (item.getTitle().equals(logout0)) {
 			// Anchor.getInstance().setCurrentUser(null);//NOTE THAT SETTING THE
 			// USER TO NULL CAUSES THE PROGRAM TO CRASH IF THE USER HITS THE
 			// BACK BUTTON AFTER LOGGING OUT
-		}
-		this.startActivity(item.getIntent());
-		return true;
-	}
+        }
+        this.startActivity(item.getIntent());
+        return true;
+    }
 
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
 	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-		public SectionsPagerAdapter(FragmentManager fm) {
+    	/**
+    	 * SectionsPagerAdapter.
+    	 * 
+    	 * @param fm fragment manager
+    	 */
+        public SectionsPagerAdapter(FragmentManager fm) {
 			super(fm);
-		}
+        }
 
-		@Override
-		public Fragment getItem(int position) {
+        @Override
+        public Fragment getItem(int position) {
 			// getItem is called to instantiate the fragment for the given page.
 			// Return a DummySectionFragment (defined as a static inner class
 			// below) with the page number as its lone argument.
-			Fragment fragment;
-			if (position == 0) {
-				fragment = new AccountListFragment();
-			} else if (position == 1) {
-				fragment = new TransactionHistoryFragment();
-			} else {
-				fragment = new ReportFragment();
+            Fragment fragment;
+            if (position == 0) {
+                fragment = new AccountListFragment();
+            }
+            else if (position == 1) {
+                fragment = new TransactionHistoryFragment();
+            }
+            else {
+                fragment = new ReportFragment();
 				// Bundle args = new Bundle();
 				// args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position
 				// + 1);
 				// fragment.setArguments(args);
-			}
-			return fragment;
-		}
+            }
+            return fragment;
+        }
 
-		@Override
-		public int getCount() {
+        @Override
+        public int getCount() {
 			// Show 3 total pages.
-			return 3;
-		}
+            return 3;
+        }
 
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_accounts).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_history).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_reports).toUpperCase(l);
-			}
-			return null;
-		}
-	}
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_accounts).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_history).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_reports).toUpperCase(l);
+            }
+            return null;
+        }
+    }
 
 	/**
 	 * A dummy fragment representing a section of the app, but that simply
 	 * displays dummy text.
 	 */
-	public static class DummySectionFragment extends Fragment {
+    public static class DummySectionFragment extends Fragment {
 		/**
 		 * The fragment argument representing the section number for this
 		 * fragment.
 		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
+        public static final String ARG_SECTION_NUMBER = "section_number";
 
-		public DummySectionFragment() {
-		}
+        /**
+         * DummySectionFragment. Maybe we don't need it? Who knows.
+         */
+        public DummySectionFragment() {
+        }
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_home_dummy,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home_dummy, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
 
 }
