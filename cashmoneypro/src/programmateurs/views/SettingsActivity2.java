bddc14b506e5901d0efcad9c88347a5ab4becf80
Sara-Cagle <cagle.sara@gmail.com>
2014-04-02 22:35:01 -0400

    ReportFragment, SettingsActivity2, TransactionAdapter checkstyle

--- cashmoneypro/src/programmateurs/views/SettingsActivity2.java
+++ cashmoneypro/src/programmateurs/views/SettingsActivity2.java
@@ -22,8 +22,6 @@ import android.support.v4.app.NavUtils;
 import java.util.List;
 
 import net.programmateurs.R;
-import net.programmateurs.R.string;
-import net.programmateurs.R.xml;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -43,29 +41,29 @@ public class SettingsActivity2 extends PreferenceActivity {
 	 * as a master/detail two-pane view on tablets. When true, a single pane is
 	 * shown on tablets.
 	 */
-	private static final boolean ALWAYS_SIMPLE_PREFS = false;
+    private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setupActionBar();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
 
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			// Show the Up button in the action bar.
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
 			// This ID represents the Home or Up button. In the case of this
 			// activity, the Up button is shown. Use NavUtils to allow users
 			// to navigate up one level in the application structure. For
@@ -75,69 +73,75 @@ public class SettingsActivity2 extends PreferenceActivity {
 			//
 			// TODO: If Settings has multiple levels, Up should navigate up
 			// that hierarchy.
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
 
-		setupSimplePreferencesScreen();
-	}
+        setupSimplePreferencesScreen();
+    }
 
 	/**
 	 * Shows the simplified settings UI if the device configuration if the
 	 * device configuration dictates that a simplified, single-pane UI should be
 	 * shown.
 	 */
-	private void setupSimplePreferencesScreen() {
-		if (!isSimplePreferences(this)) {
-			return;
-		}
+    @SuppressWarnings("deprecation")
+    private void setupSimplePreferencesScreen() {
+        if (!isSimplePreferences(this)) {
+            return;
+        }
 
 		// In the simplified UI, fragments are not used at all and we instead
 		// use the older PreferenceActivity APIs.
 
 		// Add 'general' preferences.
-		addPreferencesFromResource(R.xml.pref_general);
+        addPreferencesFromResource(R.xml.pref_general);
 
 		// Add 'notifications' preferences, and a corresponding header.
-		PreferenceCategory fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_notifications);
-		getPreferenceScreen().addPreference(fakeHeader);
-		addPreferencesFromResource(R.xml.pref_notification);
+        PreferenceCategory fakeHeader = new PreferenceCategory(this);
+        fakeHeader.setTitle(R.string.pref_header_notifications);
+        getPreferenceScreen().addPreference(fakeHeader);
+        addPreferencesFromResource(R.xml.pref_notification);
 
 		// Add 'data and sync' preferences, and a corresponding header.
-		fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_data_sync);
-		getPreferenceScreen().addPreference(fakeHeader);
-		addPreferencesFromResource(R.xml.pref_data_sync);
+        fakeHeader = new PreferenceCategory(this);
+        fakeHeader.setTitle(R.string.pref_header_data_sync);
+        getPreferenceScreen().addPreference(fakeHeader);
+        addPreferencesFromResource(R.xml.pref_data_sync);
 
 		// Bind the summaries of EditText/List/Dialog/Ringtone preferences to
 		// their values. When their values change, their summaries are updated
 		// to reflect the new value, per the Android Design guidelines.
-		bindPreferenceSummaryToValue(findPreference("example_text"));
-		bindPreferenceSummaryToValue(findPreference("example_list"));
-		bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
-		bindPreferenceSummaryToValue(findPreference("sync_frequency"));
-	}
+        //CHECKSTYLE:OFF
+        bindPreferenceSummaryToValue(findPreference("example_text"));
+        bindPreferenceSummaryToValue(findPreference("example_list"));
+        bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
+        bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        //CHECKSTYLE:ON
+    }
 
 	/** {@inheritDoc} */
-	@Override
-	public boolean onIsMultiPane() {
-		return isXLargeTablet(this) && !isSimplePreferences(this);
-	}
+    @Override
+    public boolean onIsMultiPane() {
+        return isXLargeTablet(this) && !isSimplePreferences(this);
+    }
 
 	/**
 	 * Helper method to determine if the device has an extra-large screen. For
 	 * example, 10" tablets are extra-large.
+	 * 
+	 * @param context context
+	 * @return boolean boolean
 	 */
-	private static boolean isXLargeTablet(Context context) {
-		return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-	}
+    private static boolean isXLargeTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    }
 
 	/**
 	 * Determines whether the simplified settings UI should be shown. This is
@@ -145,73 +149,78 @@ public class SettingsActivity2 extends PreferenceActivity {
 	 * doesn't have newer APIs like {@link PreferenceFragment}, or the device
 	 * doesn't have an extra-large screen. In these cases, a single-pane
 	 * "simplified" settings UI should be shown.
+	 * 
+	 * @param context context
+	 * @return boolean boolean
 	 */
-	private static boolean isSimplePreferences(Context context) {
-		return ALWAYS_SIMPLE_PREFS
+    private static boolean isSimplePreferences(Context context) {
+        return ALWAYS_SIMPLE_PREFS
 				|| Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
 				|| !isXLargeTablet(context);
-	}
+    }
 
 	/** {@inheritDoc} */
-	@Override
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public void onBuildHeaders(List<Header> target) {
-		if (!isSimplePreferences(this)) {
-			loadHeadersFromResource(R.xml.pref_headers, target);
-		}
-	}
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        if (!isSimplePreferences(this)) {
+            loadHeadersFromResource(R.xml.pref_headers, target);
+        }
+    }
 
 	/**
 	 * A preference value change listener that updates the preference's summary
 	 * to reflect its new value.
 	 */
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
 
-			if (preference instanceof ListPreference) {
+            if (preference instanceof ListPreference) {
 				// For list preferences, look up the correct display value in
 				// the preference's 'entries' list.
-				ListPreference listPreference = (ListPreference) preference;
-				int index = listPreference.findIndexOfValue(stringValue);
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
 
 				// Set the summary to reflect the new value.
-				preference
-						.setSummary(index >= 0 ? listPreference.getEntries()[index]
-								: null);
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
 
-			} else if (preference instanceof RingtonePreference) {
+            }
+            else if (preference instanceof RingtonePreference) {
 				// For ringtone preferences, look up the correct display value
 				// using RingtoneManager.
-				if (TextUtils.isEmpty(stringValue)) {
+                if (TextUtils.isEmpty(stringValue)) {
 					// Empty values correspond to 'silent' (no ringtone).
-					preference.setSummary(R.string.pref_ringtone_silent);
+                    preference.setSummary(R.string.pref_ringtone_silent);
 
-				} else {
-					Ringtone ringtone = RingtoneManager.getRingtone(
+                }
+                else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(
 							preference.getContext(), Uri.parse(stringValue));
 
-					if (ringtone == null) {
+                    if (ringtone == null) {
 						// Clear the summary if there was a lookup error.
-						preference.setSummary(null);
-					} else {
+                        preference.setSummary(null);
+                    }
+                    else {
 						// Set the summary to reflect the new ringtone display
 						// name.
-						String name = ringtone
+                        String name = ringtone
 								.getTitle(preference.getContext());
-						preference.setSummary(name);
-					}
-				}
+                        preference.setSummary(name);
+                    }
+                }
 
-			} else {
+            }
+            else {
 				// For all other preferences, set the summary to the value's
 				// simple string representation.
-				preference.setSummary(stringValue);
-			}
-			return true;
-		}
-	};
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
 
 	/**
 	 * Binds a preference's summary to its value. More specifically, when the
@@ -221,77 +230,76 @@ public class SettingsActivity2 extends PreferenceActivity {
 	 * dependent on the type of preference.
 	 * 
 	 * @see #sBindPreferenceSummaryToValueListener
+	 * @param preference preference
 	 */
-	private static void bindPreferenceSummaryToValue(Preference preference) {
+    private static void bindPreferenceSummaryToValue(Preference preference) {
 		// Set the listener to watch for value changes.
-		preference
-				.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
 		// Trigger the listener immediately with the preference's
 		// current value.
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(
-				preference,
+        sBindPreferenceSummaryToValueListener.onPreferenceChange( preference,
 				PreferenceManager.getDefaultSharedPreferences(
 						preference.getContext()).getString(preference.getKey(),
 						""));
-	}
+    }
 
 	/**
 	 * This fragment shows general preferences only. It is used when the
 	 * activity is showing a two-pane settings UI.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class GeneralPreferenceFragment extends PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_general);
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
 
 			// Bind the summaries of EditText/List/Dialog/Ringtone preferences
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("example_text"));
-			bindPreferenceSummaryToValue(findPreference("example_list"));
-		}
-	}
+            bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("example_list"));
+        }
+    }
 
 	/**
 	 * This fragment shows notification preferences only. It is used when the
 	 * activity is showing a two-pane settings UI.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class NotificationPreferenceFragment extends
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class NotificationPreferenceFragment extends
 			PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_notification);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_notification);
 
 			// Bind the summaries of EditText/List/Dialog/Ringtone preferences
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
-		}
-	}
+            bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
+        }
+    }
 
 	/**
 	 * This fragment shows data and sync preferences only. It is used when the
 	 * activity is showing a two-pane settings UI.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class DataSyncPreferenceFragment extends PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_data_sync);
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class DataSyncPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_data_sync);
 
 			// Bind the summaries of EditText/List/Dialog/Ringtone preferences
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("sync_frequency"));
-		}
-	}
+            bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        }
+    }
 }
--- cashmoneypro/src/programmateurs/views/TransactionAdapter.java
+++ cashmoneypro/src/programmateurs/views/TransactionAdapter.java
@@ -23,77 +23,87 @@ import java.util.Locale;
 
 /**
  * I'd ask Currell for more information on how this class works Basically it
- * make the view that fits inside a ListView object
+ * make the view that fits inside a ListView object.
  * 
  * @author currell
  * @version 0.2
  * 
  */
 public class TransactionAdapter extends BaseAdapter {
+	//CHECKSTYLE:OFF
 	private NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
 	private final Activity activity;
 	List<Transaction> transactions;
+	//CHECKSTYLE:ON
 
-	public TransactionAdapter(Activity activity, List<Transaction> transactions) {
-		this.activity = activity;
-		this.transactions = transactions;
-	}
+/**
+ * Transaction adapter.
+ * 
+ * @param activity activity
+ * @param transactions transactions
+ */
+	//CHECKSTYLE:OFF
+    public TransactionAdapter(Activity activity, List<Transaction> transactions) {
+        this.activity = activity;
+        this.transactions = transactions;
+    }
+    //CHECKSTYLE:ON
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-		LayoutInflater inflater = (LayoutInflater) activity
+        LayoutInflater inflater = (LayoutInflater) activity
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.unused_corqitemlayout, parent,
-				false);
-		TextView header = (TextView) rowView.findViewById(R.id.corqHeader);
-		TextView body = (TextView) rowView.findViewById(R.id.corqText);
-		ImageView imageView = (ImageView) rowView.findViewById(R.id.corqImage);
-		final Transaction q = transactions.get(position);
-		String headerText = q.getTransactionName();
+        View rowView = inflater.inflate(R.layout.unused_corqitemlayout, parent, false);
+        TextView header = (TextView) rowView.findViewById(R.id.corqHeader);
+        TextView body = (TextView) rowView.findViewById(R.id.corqText);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.corqImage);
+        final Transaction q = transactions.get(position);
+        String headerText = q.getTransactionName();
 
-		String bodyText = q.getTransactionType() + " of "
-				+ nf.format(q.getTransactionAmountAsDouble());
-		bodyText += "\n" + q.getTransactionComment();
+        String bodyText = q.getTransactionType() + " of " + nf.format(q.getTransactionAmountAsDouble());
+        bodyText += "\n" + q.getTransactionComment();
 
-		header.setText(headerText);
-		body.setText(bodyText);
+        header.setText(headerText);
+        body.setText(bodyText);
 
-		if (q.getTransactionType() == TRANSACTION_TYPE.DEPOSIT) {
-			imageView.setImageResource(R.drawable.circle_green);
-		} else if (q.getTransactionType() == TRANSACTION_TYPE.WITHDRAWAL) {
-			imageView.setImageResource(R.drawable.circle_red);
-		} else {
-			imageView.setImageResource(R.drawable.circle_orange);
-		}
+        if (q.getTransactionType() == TRANSACTION_TYPE.DEPOSIT) {
+            imageView.setImageResource(R.drawable.circle_green);
+        }
+        else if (q.getTransactionType() == TRANSACTION_TYPE.WITHDRAWAL) {
+            imageView.setImageResource(R.drawable.circle_red);
+        }
+        else {
+            imageView.setImageResource(R.drawable.circle_orange);
+        }
 
 		// onClickListener for each view
 		// We can change this to go to an Activity when pressed later.
-		rowView.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				Anchor.getInstance()
+        rowView.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Anchor.getInstance()
 						.showDialog(activity,
 								q.getTransactionName() + ": Details",
 								q.formatDetails());
-			}
-		});
+            }
+        });
 
-		return rowView;
-	}
+        return rowView;
+    }
 
-	@Override
-	public int getCount() {
-		return transactions.size();
-	}
+    @Override
+    public int getCount() {
+        return transactions.size();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return transactions.get(position);
-	}
+    @Override
+    public Object getItem(int position) {
+        return transactions.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
 }
