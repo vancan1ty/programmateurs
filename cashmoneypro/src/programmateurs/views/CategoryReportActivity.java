2cd0eade935b13c14bd3f17c6d8d592e0e8646d3
Sara-Cagle <cagle.sara@gmail.com>
2014-04-02 21:16:50 -0400

    finished CategoryReportActivity checkstyle

--- cashmoneypro/src/programmateurs/views/CategoryReportActivity.java
+++ cashmoneypro/src/programmateurs/views/CategoryReportActivity.java
@@ -8,59 +8,63 @@ import programmateurs.models.Anchor;
 import programmateurs.models.RealDataSource;
 
 import net.programmateurs.R;
-import net.programmateurs.R.layout;
-import net.programmateurs.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.TextView;
 
+/**
+ * 
+ * @author programmateurs
+ *
+ */
 public class CategoryReportActivity extends Activity {
 
+	//CHECKSTYLE:OFF
 	private RealDataSource src;
 	TextView textReport;
 	Calendar startCalendar;
 	Calendar endCalendar;
 	Transaction.TRANSACTION_TYPE reportType;
 	Anchor anchor;
+	//CHECKSTYLE:ON
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		anchor = Anchor.getInstance();
-		setContentView(R.layout.activity_category_report);
-		Bundle extras = getIntent().getExtras();
-		startCalendar = (Calendar) extras.getSerializable("startCalendar");
-		endCalendar = (Calendar) extras.getSerializable("endCalendar");
-		reportType = (TRANSACTION_TYPE) extras.getSerializable("reportType");
-		src = new RealDataSource(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        anchor = Anchor.getInstance();
+        setContentView(R.layout.activity_category_report);
+        Bundle extras = getIntent().getExtras();
+        startCalendar = (Calendar) extras.getSerializable("startCalendar");
+        endCalendar = (Calendar) extras.getSerializable("endCalendar");
+        reportType = (TRANSACTION_TYPE) extras.getSerializable("reportType");
+        src = new RealDataSource(this);
 
-		textReport = (TextView) findViewById(R.id.text_cat_report);
+        textReport = (TextView) findViewById(R.id.text_cat_report);
 
-	}
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-		src.open();
-		textReport.setText(src.getCategoryReport(startCalendar, endCalendar,
+        src.open();
+        textReport.setText(src.getCategoryReport(startCalendar, endCalendar,
 				reportType, anchor.getCurrentUser().getUserID()));
+    }
 
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-	@Override
-	protected void onPause() {
-		super.onPause();
+        src.close();
+    }
 
-		src.close();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.category_report, menu);
-		return true;
-	}
+        getMenuInflater().inflate(R.menu.category_report, menu);
+        return true;
+    }
 
 }
