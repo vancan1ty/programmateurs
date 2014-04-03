5e883db8fa137f078488cd059c7ff43adb2ffc8a
Sara-Cagle <cagle.sara@gmail.com>
2014-04-02 21:22:45 -0400

    checkstyle for ConfirmDialog save for 1

--- cashmoneypro/src/programmateurs/views/ConfirmDialog.java
+++ cashmoneypro/src/programmateurs/views/ConfirmDialog.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
-import android.widget.EditText;
 
 /**
  * Helper for creating dialogs that prompt for confirmation. Presents text with
@@ -15,49 +14,52 @@ import android.widget.EditText;
  * @version 0.1
  */
 public abstract class ConfirmDialog extends AlertDialog.Builder implements
-		OnClickListener {
+    OnClickListener {
 
 	/**
 	 * Creates new ConfirDialog for given context using provided title and
 	 * message.
 	 * 
-	 * @param context
+	 * @param context context
+	 * @param message message
+	 * @param title title
 	 */
-	public ConfirmDialog(Context context, String title, String message) {
-		super(context);
-		setTitle(title);
-		setMessage(message);
+    public ConfirmDialog(Context context, String title, String message) {
+        super(context);
+        setTitle(title);
+        setMessage(message);
 
-		setPositiveButton("ok", this);
-		setNegativeButton("cancel", this);
-	}
+        setPositiveButton("ok", this);
+        setNegativeButton("cancel", this);
+    }
 
 	/**
 	 * will be called when "cancel" pressed. closes the dialog. can be
 	 * overridden.
 	 * 
-	 * @param dialog
+	 * @param dialog dialog
 	 */
-	public void onCancelClicked(DialogInterface dialog) {
-		dialog.dismiss();
-	}
+    public void onCancelClicked(DialogInterface dialog) {
+        dialog.dismiss();
+    }
 
-	@Override
-	public void onClick(DialogInterface dialog, int which) {
-		if (which == DialogInterface.BUTTON_POSITIVE) {
-			if (onOkClicked("")) {
-				dialog.dismiss();
-			}
-		} else {
-			onCancelClicked(dialog);
-		}
-	}
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            if (onOkClicked("")) {
+                dialog.dismiss();
+            }
+        }
+        else {
+            onCancelClicked(dialog);
+        }
+    }
 
 	/**
 	 * called when "ok" pressed.
 	 * 
-	 * @param input
+	 * @param input input
 	 * @return true, if the dialog should be closed. false, if not.
 	 */
-	abstract public boolean onOkClicked(String input);
+    public abstract boolean onOkClicked(String input);
 }
