dfe056afc96ef1ecb3dfde1e362deced9d2c4d9c
Currell Berry <currellberry@gmail.com>
2014-04-04 12:26:37 -0400

    commented out artificialdatasource tests since I commented that whole class for checkstyle reasons.

---
+++ cashmoneypro/src/programmateurs/models/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains the data access layer for our application.  All interaction with
+ * the database should go through classes in this package.
+ *
+ *  @author vancan1ty
+ *
+ */
+package programmateurs.models;
